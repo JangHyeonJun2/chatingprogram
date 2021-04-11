4615de94599cfa0f4704552c3240d53989fb61a4
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-04-12 01:59:33 +0900

    Initial commit

---
+++ src/main/java/com/jangjangyi/chatprogram/chatingprogram/ChatingprogramApplication.java
@@ -0,0 +1,13 @@
+package com.jangjangyi.chatprogram.chatingprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatingprogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatingprogramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jangjangyi/chatprogram/chatingprogram/ChatingprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangjangyi.chatprogram.chatingprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatingprogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
